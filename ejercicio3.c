d4ddc4ba4f3bfc02f142ad0a07a8993d74c9b176
OdyAuri <ody.loa100@gmail.com>
2026-04-16 14:24:05 -0600

    Update ejercicio3.c

--- ejercicio3.c
+++ ejercicio3.c
@@ -5,6 +5,7 @@ int main ()
     char *ap, c = 'a'; // Se declara el apuntador ap de tipo alfanumérico (Localidad en el espacio de memoria)
     // * es un apuntador, los apuntadores señalan un momento en especifico 
     ap = &c; //Se le asigna al apuntador la dirección de memoria de la variable c 
+    // %c de caracter
     // el gatito se usa en los caracteres que no se imprimen pero si estan en el codigo
     printf("Carácter: %c\n",*ap); 
     //Se imprime el contenido de la variable a la que apunta el apuntador ap 
